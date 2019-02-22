455a8e4177158a1a33558f0f3724bcf6a0421b14
Namio Evangelista Cavalcante Sousa <namio@proabakus.com.br>
2019-02-21 22:30:55 -0300

    UPD: Atualização da versão

--- bradocs4py/__init__.py
+++ bradocs4py/__init__.py
@@ -7,7 +7,8 @@ from .cpf import CPF, ValidadorCpf, GeradorCpf
 from .inscricaosuframa import InscricaoSuframa, ValidadorSuframa, GeradorSuframa
 from .inscricaoestadual import InscricaoEstadual, ValidadorInscricaoEstadual
 from .chaveacessonfe import ChaveAcessoNFe, ValidadorChaveAcessoNFe, GeradorChaveAcessoNFe, validarChaveAcessoNFe, gerarChaveAcessoNFe
+from .gtin import GTIN, ValidadorGTIN, GeradorGTIN, gerar_gtin, validar_gtin
 
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 
 
